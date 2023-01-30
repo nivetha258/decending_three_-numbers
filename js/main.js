e173506b07bf2013a9a92faf948a99ce615a0d67
nivetha <nivetharajendran1994@gmail.com>
2023-01-30 22:01:02 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+document.getElementById("result-section").style.display="none";
+
+let number1Ele=document.getElementById("number1")
+let number2Ele=document.getElementById("number2")
+let number3Ele=document.getElementById("number3")
+
+
+let buttonEle = document.getElementById("button");
+
+buttonEle.addEventListener("click",function (){
+
+let  number1 = parseInt(number1Ele.value);
+let  number2 = parseInt(number2Ele.value);
+let  number3 = parseInt(number3Ele.value);
+
+
+if(number1 > number2 && number1 > number3 ){
+	
+	if(number2 > number3){
+		document.getElementById("result").innerText = number1+" , "+number2+" , "+number3;
+	}
+	else{
+		document.getElementById("result").innerText = number1+" , "+number3+" , "+number2;
+	}
+}
+else if(number2 > number1 && number2 > number3 ){
+	
+	if(number1 > number3){
+		
+		document.getElementById("result").innerText = number2+" , "+number1+" , "+number3;
+	}
+	else{
+		document.getElementById("result").innerText = number2 +" , "+number3+" , "+number1;
+	}
+}
+else {
+	
+	if(number2 > number1){
+		document.getElementById("result").innerText = number3+" , "+number2+" , "+number1
+	}
+	else{
+		document.getElementById("result").innerText = number3+" , "+number1+" , "+number2;
+	}	
+}
+document.getElementById("result-section").style.display="block";
+});
